9dcfecd24e13b703142db23137f17b5067d6c21b
wacce <wapce@bol.com.br>
2021-06-01 15:38:21 -0300

    Feito animações no placar

--- public/js/placar.js
+++ public/js/placar.js
@@ -1,3 +1,5 @@
+$("#botao-placar").click(mostraPlacar);
+
 function inserePlacar() {
   var corpoTabela = $(".placar").find("tbody"); //Busca um tbody dentro de placar
   var usuario = "Douglas";
@@ -5,10 +7,22 @@ function inserePlacar() {
 
   var linha = novaLinha(usuario,numPalavras);
   linha.find(".botao-remover").click(removeLinha);
+
   corpoTabela.append(linha);//com o append é adicionado no final
   //se fosse com o prepend seria adicioando no inicio
+$(".placar").slideDown(500);//quando termina a partida ele mostra o placar
+}
+
+function scrollPlacar() {
+    var posicaoPlacar = $(".placar").offset().top; //dá a posição com relação ao topo de placar
+    $("body").animate( //faz animação para descer a página
+    {
+      scrollTop: posicaoPlacar+"px"
+
+    },1000)
 
 }
+
 function novaLinha(usuario,palavras) {
   var linha = $("<tr>");
   var colunaUsuario = $("<td>").text(usuario);
@@ -32,5 +46,14 @@ function novaLinha(usuario,palavras) {
 
 function removeLinha() {
   event.preventDefault();
-  $(this).parent().parent().remove();
+  var linha = $(this).parent().parent();
+  linha.fadeOut(1000);
+  setTimeout(function () {
+    linha.remove();
+  },1000);
+}
+
+function mostraPlacar() {
+  //$(".placar").css("display","block")//tem .show e .hide do jquery que faz isso também tem a toggle
+  $(".placar").stop().slideToggle(800);//.stop() é se for clicado varias vezes lo lugar de acumular clickcs ele para e passa para o próximo
 }
