de6d9475c755f2a5b5465dfb4ea6f6e55afe169a
wacce <wapce@bol.com.br>
2021-06-02 12:58:30 -0300

    Tratando erros e melhorando a UX

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,14 +1,25 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria() {
+  $("#spinner").show();
   $.get("http://localhost:3000/frases",trocaFraseAleatoria)//data tem o retorno da requisição feita pelo $.get
-
+  .fail(function() {//.fail trata o erro que é retornado
+    $("#erro").show();
+    setTimeout(function() {
+      $("#erro").toggle();
+    },2000);
+  })
+  .always(function() {
+    $("#spinner").toggle();
+  });
 }
 
 function trocaFraseAleatoria(data) {
   var frase = $(".frase");
-  var numeroAleatorio = Math.floor(Math.randon() * data.length);
+  var random = Math.random();
+  var numeroAleatorio = Math.floor(random * data.length);
   frase.text(data[numeroAleatorio].texto);
-  atualizaTamanhoFrase(data[numeroAleatorio].tempo);
+  atualizaTamanhoFrase();
+  atualizaTempoInicial(data[numeroAleatorio].tempo);
 
 }
